ac6601720865baf0a433d94df2b6149ba1e6f690
Ivan Constantino <icaivan11@gmail.com>
2017-03-27 04:01:39 -0700

    first commit

---
+++ js/controllers/list.js
@@ -0,0 +1,10 @@
+(function(){
+    angular
+        .module('manchesterFacts')
+        .controller('listCtrl', ListController)
+
+
+        function ListController(){
+            
+        }
+})();
